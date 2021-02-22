57f185395149caa9d9b2bbc1f814c6f7f7a8c4e6
victorhelopes <victorhelopes@gmail.com>
2021-02-22 15:32:38 -0300

    Aula 01

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express, { response, Router } from 'express';
+
+const app = express();
+
+app.get("/",(request, response)=>{
+    return response.json({ message : "Hello World - NLW04"});
+});
+
+app.post("/", (request, response) => {
+    //Recebeu os dados para salvar
+    return response.json({ message : "os dados foram salvos com sucesso" });
+})
+
+app.listen(3333);
