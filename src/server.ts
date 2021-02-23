d1e6760e7de48e348fa96c2037ca08d59d1b76ef
victorhelopes <victorhelopes@gmail.com>
2021-02-23 10:29:20 -0300

    Aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
-import express, { response, Router } from 'express';
+import 'reflect-metadata';
+import express from 'express';
+import './database';
+import { router } from './routes'
 
 const app = express();
 
-app.get("/",(request, response)=>{
-    return response.json({ message : "Hello World - NLW04"});
-});
-
-app.post("/", (request, response) => {
-    //Recebeu os dados para salvar
-    return response.json({ message : "os dados foram salvos com sucesso" });
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(3333);
